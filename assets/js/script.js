4b1a11b33b23ffa20726b224071a7c7506a197f7
Jason Horgan <jasonahorgan@gmail.com>
2024-04-08 22:38:40 +0000

    Update class name for UFC logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,4 +92,11 @@ const questions = [
     }, 
 ];
 
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
 
