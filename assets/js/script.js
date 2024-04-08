65967a2f77614a7200130291b2596b83db1cd951
Jason Horgan <jasonahorgan@gmail.com>
2024-04-08 21:02:11 +0000

    rearranged html structure to simplify it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 console.log("Hello, world!");
 
+const question = document.getElementById("question_text");
+const optionA = document.getElementById("optionA");
+const optionB = document.getElementById("optionB");
+const optionC = document.getElementById("optionC");
+const optionD = document.getElementById("optionD");
+
+
+
 /** QUESTIONS */
 
 let questions = [
@@ -75,3 +83,22 @@ let questions = [
         correct: "C"
     }, 
 ];
+
+
+/**Variables for questions and score*/
+
+let lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+
+/**Function to render questions*/
+function renderQuestion() {
+
+    let q = questions[runningQuestion];
+
+    optionA.innerHTML = q.optionA;
+    optionB.innerHTML = q.optionB;
+    optionC.innerHTML = q.optionC;
+    optionD.innerHTML = q.optionD;
+} 
+
+renderQuestion();
